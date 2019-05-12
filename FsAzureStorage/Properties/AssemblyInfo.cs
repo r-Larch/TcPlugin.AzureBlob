2c28d2f674fab70ad897e43dfa0bfb50208c3653
René Larch <renelarch@gmail.com>
2019-05-12 17:58:02 +0200

    InternalsVisibleTo test project

--- FsAzureStorage/Properties/AssemblyInfo.cs
+++ FsAzureStorage/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 using System.Reflection;
 using System.Resources;
+using System.Runtime.CompilerServices;
 
 
 // this is needed for embedded resources!
 [assembly: NeutralResourcesLanguage("en-US")]
+[assembly: InternalsVisibleTo("FsAzureStorage.Test")]
